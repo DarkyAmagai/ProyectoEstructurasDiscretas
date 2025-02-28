61643862c5c140480a6622054f8b1428e6f6b0b0
DarkyAmagai <darkyamagai@outlook.es>
2025-02-27 22:35:54 -0600

    Finally, I fixed the margin thing

--- src/pages/algoritmos/tablasdeverdad.jsx
+++ src/pages/algoritmos/tablasdeverdad.jsx
@@ -30,6 +30,13 @@ export default function Home() {
         setExpression(expression + text);
     }
 
+    const ExpressionButton = ({expression}) => {
+        return (
+            <button className={GlobalStyles.button} onClick={() => add_expression_text(expression)}>
+                <span style={{textAlign: 'Center', justifyContent: 'center'}}>{expression}</span>
+            </button>
+        )
+    }
 
     return (
         <div className={GlobalStyles.mainContainer}>
@@ -39,19 +46,38 @@ export default function Home() {
                 justifyContent: 'left',
                 width: '100%'
             }}>
-                <Link style={{marginTop: '10px', marginLeft: '10px'}} href={'/'}>Inicio</Link>
+                <Link style={{marginTop: '2%', marginLeft: '2.5%'}} href={'/'}>Inicio</Link>
             </div>
-            <label>
-                Introduce la expresión: <input value={expression} onChange={e => setExpression(e.target.value)}></input>
+            <label style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                width: '100%',
+                marginTop: '20px',
+                alignItems: 'center'
+            }}>
+                <h1 style={{
+                    margin: '10px 10px'
+                }}
+                    className={GlobalStyles.multicolor} style={{fontSize: '3rem', marginBottom: '20px'}}>TABLAS DE
+                    VERDAD</h1>
+                <input className={GlobalStyles.inputText} value={expression}
+                       onChange={e => setExpression(e.target.value)}></input>
             </label>
             <div style={{
                 display: 'flex',
                 width: '100%',
                 flexAlign: 'row',
                 justifyContent: 'center',
-                itemAlign: 'center'
+                itemAlign: 'center',
+                gap: '10px'
             }}>
-                <button className={GlobalStyles.button} onClick={() => add_expression_text('->')}>texto</button>
+                <ExpressionButton expression={'¬'}></ExpressionButton>
+                <ExpressionButton expression={'∧'}></ExpressionButton>
+                <ExpressionButton expression={'∨'}></ExpressionButton>
+                <ExpressionButton expression={'⊕'}></ExpressionButton>
+                <ExpressionButton expression={'↔'}></ExpressionButton>
+                <ExpressionButton expression={'→'}></ExpressionButton>
             </div>
             {
                 creador_tabla()
