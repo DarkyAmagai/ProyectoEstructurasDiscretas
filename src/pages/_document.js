2ccf95ff9d6bda5b9a38c5a46b5d036b9948e848
DarkyAmagai <darkyamagai@outlook.es>
2025-04-02 09:42:47 -0600

    Mejorada la version para celular

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,14 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import {Head, Html, Main, NextScript} from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+      <Html lang="es">
+          <Head>
+              <meta name="viewport"
+                    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+              <meta name="theme-color" content="#6a5acd"/>
+              <meta name="description" content="Algoritmos matemáticos interactivos"/>
+          </Head>
       <body>
         <Main />
         <NextScript />
